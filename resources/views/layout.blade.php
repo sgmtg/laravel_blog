ff9a28899bd0810cf1a8c4dfae7c0472d088e179
Gen Sugimoto <shanbenyan3@gmail.com>
2023-03-18 13:34:38 +0900

    [fix] cssのパス asset関数

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
     <script src="/js/app.js" defer></script>
 </head>
 
