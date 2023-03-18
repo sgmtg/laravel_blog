159424f69d34704485852ba2af6da5e5c5c57290
Gen Sugimoto <shanbenyan3@gmail.com>
2023-03-18 13:43:36 +0900

    [fix] jsのパス asset関数

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-    <script src="/js/app.js" defer></script>
+    <script src="{{asset('/js/app.js')}}" defer></script>
 </head>
 
 <body>
